d819db7e6207b62ec010c9871861663ffbb196d7
tvvmvn <tvvmvn@gmail.com>
2023-12-17 23:23:18 +0900

    12/17

--- lab/main.js
+++ lab/main.js
@@ -1,79 +1,31 @@
 (function () {
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
-
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
-  canvas.style.backgroundColor = "#333";
-
+  
   /* constants */
 
-  const OFFSET_X = 20;
-  const OFFSET_Y = 20;
-  const ROW_COUNT = 9;
-  const COL_COUNT = 8;
-  const WIDTH = 460;
-  const HEIGHT = 345;
-  const CELL_WIDTH = WIDTH / COL_COUNT;
-  const CELL_HEIGHT = HEIGHT / ROW_COUNT;
+  const OFFSET = 50;
+  const COUNT = 3;
+  const SIZE = 150;
+  const CELL = SIZE / COUNT;
+
+  canvas.style.backgroundColor = "#333";
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
 
   /* variables */
 
-  var x = 0;
-  var y = 0;
-  var target;
-  var points = [];
-  var spots = [];
+  var cho = { id: 1, x: 0, y: 3, color: "green" };
+  var han = { id: 2, x: 3, y: 0, color: "red" };
+  var turn = 1;
 
-  for (var r = 0; r <= ROW_COUNT; r++) {
-    spots[r] = [];
+  setInterval(render, 10);
 
-    for (var c = 0; c <= COL_COUNT; c++) {
-      spots[r][c] = {
-        x: OFFSET_X + (c * CELL_WIDTH),
-        y: OFFSET_Y + (r * CELL_HEIGHT)
-      }
-    }
-  }
-
-  var pieces = [
-    { name: "zol", x: 0, y: 6, size: 10 },
-    { name: "zol", x: 2, y: 6, size: 10 },
-    { name: "zol", x: 4, y: 6, size: 10 },
-    { name: "zol", x: 6, y: 6, size: 10 },
-    { name: "zol", x: 8, y: 6, size: 10 },
-    { name: "po", x: 1, y: 7, size: 15 },
-    { name: "po", x: 7, y: 7, size: 15 },
-    { name: "cha", x: 0, y: 9, size: 15 },
-    { name: "cha", x: 8, y: 9, size: 15 },
-    { name: "sang", x: 1, y: 9, size: 15 },
-    { name: "sang", x: 7, y: 9, size: 15 },
-    { name: "ma", x: 2, y: 9, size: 15 },
-    { name: "ma", x: 6, y: 9, size: 15 },
-    { name: "sa", x: 3, y: 9, size: 10 },
-    { name: "sa", x: 5, y: 9, size: 10 },
-    { name: "gung", x: 4, y: 8, size: 25 },
-  ]
-
-  addEventListener("click", clickHandler);
-
-  /* run the game */
-
-  setInterval(interval, 10);
-
-  function interval() {
+  function render() {
     clearCanvas();
-    
-    move();
-    setTarget();
-    setPoints();
-
-    drawBoard();
-    drawPieces();
-    drawPoints();
-    drawCursor();
-
-    ctx.globalAlpha = 1;
+    drawGrid();
+    drawActor(han);
+    drawActor(cho);
   }
 
   function clearCanvas() {
@@ -81,238 +33,33 @@
   }
 
   /* functions */
-  
-  function setTarget() {
-    target = null;
 
-    for (var i = 0; i < pieces.length; i++) {
-      var piece = pieces[i];
-      
-      if (x == piece.x && y == piece.y) {
-        target = piece;
-      }
-    }
+  function drawActor(actor) {
+    ctx.beginPath();
+    ctx.fillStyle = actor.color;
+    ctx.arc(
+      OFFSET + (actor.x * CELL),
+      OFFSET + (actor.y * CELL),
+      15, 0, 2 * Math.PI
+    );
+    ctx.fill();
   }
 
-  function move() {
-    for (var i=0; i<points.length; i++) {
-      if (x == points[i][0] && y == points[i][1]) {
-        target.x = x;
-        target.y = y;
-
-        x = 0;
-        y = 0;
-      }
-    }
-  }
-
-  function isEmpty(x, y) {
-    var empty = true;
-
-    for (var i=0; i<pieces.length; i++) {
-      if (pieces[i].x == x && pieces[i].y == y) {
-        empty = false;
-        break;
-      }   
-    }
-
-    return empty;
-  }
-
-  function setMa() {
-    var _points = [];
-
-    // up
-    if (isEmpty(target.x, target.y - 1)) {
-      if (isEmpty(target.x -1, target.y - 2)) {
-        _points.push([target.x -1, target.y - 2]);
-      }
-      if (isEmpty(target.x + 1, target.y - 2)) {
-        _points.push([target.x + 1, target.y - 2]);
-      }
-    }
-
-    // right
-    if (isEmpty(target.x + 1, target.y)) {
-      if (isEmpty(target.x + 2, target.y - 1)) {
-        _points.push([target.x + 1, target.y - 1]);
-      }
-      if (isEmpty(target.x + 2, target.y + 1)) {
-        _points.push([target.x + 2, target.y + 1]);
-      }
-    }
-
-    // down
-    if (isEmpty(target.x, target.y + 1)) {
-      if (isEmpty(target.x + 1, target.y + 2)) {
-        _points.push([target.x + 1, target.y + 2]);
-      }
-      if (isEmpty(target.x - 1, target.y + 2)) {
-        _points.push([target.x - 1, target.y + 2]);
-      }
-    }
-
-    // left
-    if (isEmpty(target.x - 1, target.y)) {
-      if (isEmpty(target.x - 2, target.y + 1)) {
-        _points.push([target.x - 2, target.y + 1]);
-      }
-      if (isEmpty(target.x - 2, target.y - 1)) {
-        _points.push([target.x - 2, target.y - 1]);
-      }
-    }
-
-    return _points;
-  }
-
-  function setPoints() {
-    if (target) {
-      if (target.name == "zol") {
-        points = [
-          [target.x, target.y - 1],
-          [target.x - 1, target.y],
-          [target.x + 1, target.y],
-        ]
-      }
-
-      if (target.name == "ma") {
-        points = setMa()
-      }
-
-      if (target.name == "cha") {
-        var tmp = []
-
-        // vertical
-        for (var i = 0; i <= ROW_COUNT; i++) {
-          if (i == target.y) {
-            continue;
-          }
-          tmp.push([target.x, i]);
-        }
-        // horizontal
-        for (var i = 0; i <= COL_COUNT; i++) {
-          if (i == target.x) {
-            continue;
-          }
-          tmp.push([i, target.y]);
-        }
-
-        points = tmp;
-      }
-
-      if (target.name == "sang") {
-        points = [];
-      }
-
-      if (target.name == "po") {
-        points = [];
-      }
-
-      if (target.name == "sa") {
-        points = [];
-      }
-
-      if (target.name == "gung") {
-        points = [];
-      }
-    } else {
-      points = [];
-    }
-  }
-
-  /* draw */
-  
-  function drawPieces(piece) {
-    for (var i = 0; i < pieces.length; i++) {
-      var piece = pieces[i];
-  
-      ctx.beginPath();
-      ctx.arc(
-        OFFSET_X + (piece.x * CELL_WIDTH), 
-        OFFSET_Y + (piece.y * CELL_HEIGHT),
-        piece.size, 0, 2 * Math.PI
-      );
-      ctx.fillStyle = "#ddd";
-      ctx.fill();
-    }
-  }
-
-  function drawPoints() {
-    for (var i=0; i<points.length; i++) {
-      ctx.beginPath();
-      ctx.strokeStyle = "#f00";
-      ctx.lineWidth = 2;
-      ctx.arc(
-        OFFSET_X + (points[i][0] * CELL_WIDTH),
-        OFFSET_Y + (points[i][1] * CELL_HEIGHT),
-        10, 0, 2 * Math.PI
-      );
-      ctx.stroke();
-    }
-  }
-
-  function drawBoard() {
+  function drawGrid() {
     ctx.beginPath();
     ctx.strokeStyle = "#000";
     ctx.lineWidth = 1;
 
-    // grid
-    for (var r = 0; r <= ROW_COUNT; r++) {
-      ctx.moveTo(OFFSET_X, OFFSET_Y + (CELL_HEIGHT * r));
-      ctx.lineTo(OFFSET_X + WIDTH, OFFSET_Y + (CELL_HEIGHT * r));
+    for (var r = 0; r <= COUNT; r++) {
+      ctx.moveTo(OFFSET, OFFSET + (CELL * r));
+      ctx.lineTo(OFFSET + SIZE, OFFSET + (CELL * r));
     }
 
-    for (var c = 0; c <= COL_COUNT; c++) {
-      ctx.moveTo(OFFSET_X + (CELL_WIDTH * c), OFFSET_Y);
-      ctx.lineTo(OFFSET_X + (CELL_WIDTH * c), OFFSET_Y + HEIGHT);
+    for (var c = 0; c <= COUNT; c++) {
+      ctx.moveTo(OFFSET + (CELL * c), OFFSET);
+      ctx.lineTo(OFFSET + (CELL * c), OFFSET + SIZE);
     }
-
-    // cross around king area
-    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 0));
-    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 2));
-
-    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 0));
-    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 2));
-
-    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 7));
-    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 9));
-
-    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 7));
-    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 9));
 
     ctx.stroke();
   }
-
-  function drawCursor() {
-    ctx.beginPath();
-    ctx.strokeStyle = "#00f";
-    ctx.lineWidth = 2;
-    ctx.arc(
-      OFFSET_X + (x * CELL_WIDTH),
-      OFFSET_Y + (y * CELL_HEIGHT),
-      10, 0, 2 * Math.PI
-    );
-    ctx.stroke();
-  }
-
-  /* control */
-
-  function clickHandler(e) {
-    for (var r = 0; r <= ROW_COUNT; r++) {
-      for (var c = 0; c <= COL_COUNT; c++) {
-        var spot = spots[r][c]; // intersection
-
-        var a = Math.pow((e.clientX - spot.x), 2) + Math.pow((e.clientY - spot.y), 2);
-        var b = Math.pow(CELL_HEIGHT / 2, 2);
-
-        if (a <= b) {
-          // target
-          console.log("selected:", x, y);
-          x = c;
-          y = r;
-        }
-      }
-    }
-  }
-})()
-
+})();
---
+++ xiangqi/main.js
@@ -0,0 +1,357 @@
+(function () {
+  const canvas = document.getElementById("canvas");
+  const ctx = canvas.getContext("2d");
+
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  canvas.style.backgroundColor = "#333";
+
+  /* constants */
+
+  const OFFSET_X = 20;
+  const OFFSET_Y = 20;
+  const ROW_COUNT = 9;
+  const COL_COUNT = 8;
+  const WIDTH = 460;
+  const HEIGHT = 345;
+  const CELL_WIDTH = WIDTH / COL_COUNT;
+  const CELL_HEIGHT = HEIGHT / ROW_COUNT;
+  const SPOTS = [];
+
+  for (var r = 0; r <= ROW_COUNT; r++) {
+    SPOTS[r] = [];
+
+    for (var c = 0; c <= COL_COUNT; c++) {
+      SPOTS[r][c] = [
+        OFFSET_X + (c * CELL_WIDTH),
+        OFFSET_Y + (r * CELL_HEIGHT)
+      ]
+    }
+  }
+
+  /* variables */
+
+  var x = -1;
+  var y = -1;
+  var target;
+  var points = [];
+
+  var han = [
+    { name: "졸", x: 2, y: 3, size: 15, color: "#f00" },
+    { name: "졸", x: 6, y: 3, size: 15, color: "#f00" },
+    { name: "차", x: 0, y: 0, size: 20, color: "#f00" },
+    { name: "차", x: 8, y: 0, size: 20, color: "#f00" },
+    { name: "마", x: 6, y: 0, size: 20, color: "#f00" },
+    { name: "마", x: 2, y: 0, size: 20, color: "#f00" },
+    { name: "궁", x: 4, y: 1, size: 30, color: "#f00" },
+  ]
+
+  // below
+  var cho = [
+    { name: "졸", x: 2, y: 6, size: 15, color: "#0b0" },
+    { name: "졸", x: 6, y: 6, size: 15, color: "#0b0" },
+    { name: "차", x: 0, y: 9, size: 20, color: "#0b0" },
+    { name: "차", x: 8, y: 9, size: 20, color: "#0b0" },
+    { name: "마", x: 2, y: 9, size: 20, color: "#0b0" },
+    { name: "마", x: 6, y: 9, size: 20, color: "#0b0" },
+    { name: "궁", x: 4, y: 8, size: 30, color: "#0b0" },
+  ]
+
+  addEventListener("click", clickHandler);
+
+  /* run the game */
+
+  setInterval(interval, 10);
+
+  function interval() {
+    clearCanvas();
+    
+    setTarget();
+    setPoints();
+    move();
+
+    drawBoard();
+    drawPieces(han);
+    drawPieces(cho);
+    drawPoints();
+    drawCursor();
+
+    ctx.globalAlpha = 1;
+  }
+
+  function clearCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  }
+
+  /* functions */
+  
+  function setTarget() {
+    for (var i = 0; i < cho.length; i++) {
+      var piece = cho[i];
+      
+      if (x == piece.x && y == piece.y) {
+        target = piece;
+        break;
+      }
+    }
+  }
+
+  function setPoints() {
+    if (!target) {
+      return;
+    }
+
+    if (target.name == "졸") {
+      var _points = [];
+
+      if (isEmpty(target.x, target.y - 1)) {
+        _points.push([target.x, target.y - 1])
+      }
+
+      if (isEmpty(target.x - 1, target.y)) {
+        _points.push([target.x - 1, target.y])
+      }
+
+      if (isEmpty(target.x + 1, target.y)) {
+        _points.push([target.x + 1, target.y])
+      }
+
+      points = _points;
+    }
+
+    if (target.name == "마") {
+      points = getMa()
+    }
+
+    if (target.name == "차") {
+      points = getCha();
+    }
+
+    if (target.name == "sang") {
+      points = [];
+    }
+
+    if (target.name == "po") {
+      points = [];
+    }
+
+    if (target.name == "sa") {
+      points = [];
+    }
+
+    if (target.name == "궁") {
+      points = [];
+    }
+  }
+
+  function move() {
+    for (var i=0; i<points.length; i++) {
+      if (x == points[i][0] && y == points[i][1]) {
+        target.x = x;
+        target.y = y;
+      }
+    }
+  }
+
+  function getCha() {
+    var _points = [];
+
+    // up
+    for (var i = target.y - 1; i >= 0; i--) {
+      if (isEmpty(target.x, i)) {
+        _points.push([target.x, i]);
+      } else {
+        break;
+      }
+    }
+
+    // right
+    for (var i = target.x + 1; i <= COL_COUNT; i++) {
+      if (isEmpty(i, target.y)) {
+        _points.push([i, target.y]);
+      } else {
+        break;
+      }
+    }
+
+    // down
+    for (var i = target.y + 1; i <= ROW_COUNT; i++) {
+      if (isEmpty(target.x, i)) {
+        _points.push([target.x, i]);
+      } else {
+        break;
+      }
+    }
+
+    // left
+    for (var i = target.x - 1; i >= 0; i--) {
+      if (isEmpty(i, target.y)) {
+        _points.push([i, target.y]);
+      } else {
+        break;
+      }
+    }
+
+    return _points;
+  }
+
+  function getMa() {
+    var _points = [];
+
+    // up
+    if (isEmpty(target.x, target.y - 1)) {
+      if (isEmpty(target.x - 1, target.y - 2)) {
+        _points.push([target.x - 1, target.y - 2]);
+      }
+      if (isEmpty(target.x + 1, target.y - 2)) {
+        _points.push([target.x + 1, target.y - 2]);
+      }
+    }
+
+    // right
+    if (isEmpty(target.x + 1, target.y)) {
+      if (isEmpty(target.x + 2, target.y - 1)) {
+        _points.push([target.x + 2, target.y - 1]);
+      }
+      if (isEmpty(target.x + 2, target.y + 1)) {
+        _points.push([target.x + 2, target.y + 1]);
+      }
+    }
+
+    // down
+    if (isEmpty(target.x, target.y + 1)) {
+      if (isEmpty(target.x + 1, target.y + 2)) {
+        _points.push([target.x + 1, target.y + 2]);
+      }
+      if (isEmpty(target.x - 1, target.y + 2)) {
+        _points.push([target.x - 1, target.y + 2]);
+      }
+    }
+
+    // left
+    if (isEmpty(target.x - 1, target.y)) {
+      if (isEmpty(target.x - 2, target.y + 1)) {
+        _points.push([target.x - 2, target.y + 1]);
+      }
+      if (isEmpty(target.x - 2, target.y - 1)) {
+        _points.push([target.x - 2, target.y - 1]);
+      }
+    }
+
+    return _points;
+  }
+
+  function isEmpty(x, y) {
+    var empty = true;
+
+    if (x < 0 || x > COL_COUNT) {
+      return false;
+    }
+
+    if (y < 0 || y > ROW_COUNT) {
+      return false;
+    } 
+
+    for (var i=0; i<cho.length; i++) {
+      if (cho[i].x == x && cho[i].y == y) {
+        empty = false;
+        break;
+      }   
+    }
+
+    return empty;
+  }
+
+  /* draw */
+  
+  function drawPieces(pieces) {
+    for (var i = 0; i < pieces.length; i++) {
+      var piece = pieces[i];
+
+      ctx.font = piece.size + "px Arial";
+      ctx.fillStyle = piece.color;
+      ctx.fillText(
+        piece.name, 
+        OFFSET_X + (piece.x * CELL_WIDTH) - piece.size / 2.5, 
+        OFFSET_Y + (piece.y * CELL_HEIGHT) + piece.size / 3,
+      );
+    }
+  }
+
+  function drawPoints() {
+    for (var i=0; i<points.length; i++) {
+      ctx.beginPath();
+      ctx.strokeStyle = "#f00";
+      ctx.lineWidth = 2;
+      ctx.arc(
+        OFFSET_X + (points[i][0] * CELL_WIDTH),
+        OFFSET_Y + (points[i][1] * CELL_HEIGHT),
+        10, 0, 2 * Math.PI
+      );
+      ctx.stroke();
+    }
+  }
+
+  function drawBoard() {
+    ctx.beginPath();
+    ctx.strokeStyle = "#000";
+    ctx.lineWidth = 1;
+
+    // grid
+    for (var r = 0; r <= ROW_COUNT; r++) {
+      ctx.moveTo(OFFSET_X, OFFSET_Y + (CELL_HEIGHT * r));
+      ctx.lineTo(OFFSET_X + WIDTH, OFFSET_Y + (CELL_HEIGHT * r));
+    }
+
+    for (var c = 0; c <= COL_COUNT; c++) {
+      ctx.moveTo(OFFSET_X + (CELL_WIDTH * c), OFFSET_Y);
+      ctx.lineTo(OFFSET_X + (CELL_WIDTH * c), OFFSET_Y + HEIGHT);
+    }
+
+    // cross around king area
+    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 0));
+    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 2));
+
+    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 0));
+    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 2));
+
+    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 7));
+    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 9));
+
+    ctx.moveTo(OFFSET_X + (CELL_WIDTH * 5), OFFSET_Y + (CELL_HEIGHT * 7));
+    ctx.lineTo(OFFSET_X + (CELL_WIDTH * 3), OFFSET_Y + (CELL_HEIGHT * 9));
+
+    ctx.stroke();
+  }
+
+  function drawCursor() {
+    ctx.beginPath();
+    ctx.strokeStyle = "#00f";
+    ctx.lineWidth = 2;
+    ctx.arc(
+      OFFSET_X + (x * CELL_WIDTH),
+      OFFSET_Y + (y * CELL_HEIGHT),
+      10, 0, 2 * Math.PI
+    );
+    ctx.stroke();
+  }
+
+  /* control */
+
+  function clickHandler(e) {
+    for (var r = 0; r <= ROW_COUNT; r++) {
+      for (var c = 0; c <= COL_COUNT; c++) {
+        var spot = SPOTS[r][c]; // intersection
+
+        var a = Math.pow((e.clientX - spot[0]), 2) + Math.pow((e.clientY - spot[1]), 2);
+        var b = Math.pow(CELL_HEIGHT / 2, 2);
+
+        if (a <= b) {
+          x = c;
+          y = r;
+        }
+      }
+    }
+  }
+})()
+
