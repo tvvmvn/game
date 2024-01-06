204d8333a33ff8ad536b62604c2e39cfe6c8825b
tvvmvn <tvvmvn@gmail.com>
2024-01-06 16:49:31 +0900

    1/6

--- chess/main.js
+++ chess/main.js
@@ -5,15 +5,38 @@
   canvas.height = innerHeight;
   canvas.addEventListener("click", clickHandler)
 
+  /* struct */
+
+  class Piece {
+    constructor (id, name, src, team) {
+      this.id = id;
+      this.name = name;
+      this.src = src;
+      this.team = team;
+    }
+  }
+
   /* constants */
 
+  // team
+  const WHITE = 1;
+  const BLACK = 2;
+
+  // pieces
+  const KING = 0;
+  const QUEEN = 1;
+  const BISHOP = 2;
+  const KNIGHT = 3;
+  const ROOK = 4;
+  const PAWN = 5;
+
+  // board
   const OFFSET_X = 50;
   const OFFSET_Y = 50;
-  const SIZE = 200
-  const COUNT = 4
+  const SIZE = 400;
+  const COUNT = 8;
   const CELL = SIZE / COUNT;
   const CHECKER = [];
-
   for (var r = 0; r < COUNT; r++) {
     CHECKER[r] = [];
 
@@ -27,10 +50,38 @@
   }
   
   const PIECES = [
-    { id: 1, name: "pawn", team: 1 },
-    { id: 2, name: "knight", team: 1 },
-    { id: 3, name: "pawn", team: 2 },
-    { id: 4, name: "knight", team: 2 },
+    new Piece(1, KING, "♔", WHITE), // user 1
+    new Piece(2, QUEEN, "♕", WHITE),
+    new Piece(3, BISHOP, "♗", WHITE),
+    new Piece(4, BISHOP, "♗", WHITE),
+    new Piece(5, KNIGHT, "♘", WHITE),
+    new Piece(6, KNIGHT, "♘", WHITE),
+    new Piece(7, ROOK, "♖", WHITE),
+    new Piece(8, ROOK, "♖", WHITE),
+    new Piece(9, PAWN, "♙", WHITE),
+    new Piece(10, PAWN, "♙", WHITE),
+    new Piece(11, PAWN, "♙", WHITE),
+    new Piece(12, PAWN, "♙", WHITE),
+    new Piece(13, PAWN, "♙", WHITE),
+    new Piece(14, PAWN, "♙", WHITE),
+    new Piece(15, PAWN, "♙", WHITE),
+    new Piece(16, PAWN, "♙", WHITE),
+    new Piece(17, KING, "♚", BLACK), // user 2
+    new Piece(18, QUEEN, "♛", BLACK),
+    new Piece(19, BISHOP, "♝", BLACK),
+    new Piece(20, BISHOP, "♝", BLACK),
+    new Piece(21, KNIGHT, "♞", BLACK),
+    new Piece(22, KNIGHT, "♞", BLACK),
+    new Piece(23, ROOK, "♜", BLACK),
+    new Piece(24, ROOK, "♜", BLACK),
+    new Piece(25, PAWN, "♟", BLACK),
+    new Piece(26, PAWN, "♟", BLACK),
+    new Piece(27, PAWN, "♟", BLACK),
+    new Piece(28, PAWN, "♟", BLACK),
+    new Piece(29, PAWN, "♟", BLACK),
+    new Piece(30, PAWN, "♟", BLACK),
+    new Piece(31, PAWN, "♟", BLACK),
+    new Piece(32, PAWN, "♟", BLACK),
   ]
 
   /* variables */
@@ -38,8 +89,9 @@
   var board;
   var row, col;
   var prevRow, prevCol;
-  var selected;
-  var turn = 2;
+  var pieceId;
+  var targetId;
+  var turn;
   var start = false;
   var end;
   var message;
@@ -71,18 +123,22 @@
 
   function initialize() {
     board = [
-      [0, 1, 0, 2],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 3, 0, 4],
+      [23, 21, 19, 18, 17, 20, 22, 24],
+      [25, 26, 27, 28, 29, 30, 31, 32],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0],
+      [9, 10, 11, 12, 13, 14, 15, 16],
+      [7, 5, 9, 2, 1, 4, 6, 8],
     ]
     row = 0;
     col = 0;
     prevRow = row;
     prevCol = col;
-    turn = 2;
+    turn = 1;
     message = "";
-    selected = null;
+    pieceId = null;
     end = false;
   }
 
@@ -98,20 +154,6 @@
     }
 
     return piece;
-  }
-
-  function isTakeable(id) {
-    if (!id) {
-      return true;
-    }
-
-    var piece = getPieceById(id);
-
-    if (piece.team != turn) {
-      return true;
-    }
-
-    return false;
   }
 
   function setEnd() {
@@ -133,7 +175,7 @@
   }
 
   function f() {
-    if (!selected) {
+    if (!pieceId) {
       var id = board[row][col];
       
       if (id) {
@@ -141,21 +183,21 @@
         var piece = getPieceById(id);
     
         if (piece.team == turn)  {
-          selected = id;
+          pieceId = id;
         }
       }
     } else {
       if (prevRow != row || prevCol != col) {
         // case 1: change piece to move
-        var piece = getPieceById(selected);
-        var targetId = board[row][col]; // target id
+        var piece = getPieceById(pieceId);
+        targetId = board[row][col]; // target id
         var changed = false;
 
         if (targetId) {
           var target = getPieceById(targetId);
           
           if (target.team == piece.team) {
-            selected = targetId;
+            pieceId = targetId;
             changed = true;
           }
         } 
@@ -173,10 +215,10 @@
           }
 
           board[prevRow][prevCol] = 0;
-          board[row][col] = selected;
+          board[row][col] = pieceId;
     
           turn = turn == 1 ? 2 : 1;
-          selected = null;
+          pieceId = null;
         }
       }
 
@@ -187,27 +229,12 @@
   }
 
   function v() {
-    var piece = getPieceById(selected);
+    var piece = getPieceById(pieceId);
+    var target = targetId ? getPieceById(targetId) : null;
     var takeable = false;
 
-    if (piece.name == "pawn") {
+    if (piece.name == PAWN) {
       if (piece.team == 1) {
-        if (row == prevRow + 1 && col == prevCol - 1) {
-          takeable = true;
-        }
-  
-        if (row == prevRow + 1 && col == prevCol + 1) {
-          takeable = true;
-        }
-
-        if (row == prevRow + 1 && col == prevCol) {
-          if (board[row][col] == 0) {
-            takeable = true;
-          }
-        }
-      }
-
-      if (piece.team == 2) {
         if (row == prevRow - 1 && col == prevCol - 1) {
           takeable = true;
         }
@@ -222,9 +249,25 @@
           }
         }
       }
+
+      if (piece.team == 2) {
+        if (row == prevRow + 1 && col == prevCol - 1) {
+          takeable = true;
+        }
+  
+        if (row == prevRow + 1 && col == prevCol + 1) {
+          takeable = true;
+        }
+
+        if (row == prevRow + 1 && col == prevCol) {
+          if (board[row][col] == 0) {
+            takeable = true;
+          }
+        }
+      }
     } 
     
-    if (piece.name == "knight") {
+    if (piece.name == KNIGHT) {
       takeable = true;
     }
 
@@ -262,7 +305,7 @@
         if (id) {
           var piece = getPieceById(id);
 
-          if (id == selected) {
+          if (id == pieceId) {
             ctx.globalAlpha = 0.6;
           }
 
@@ -270,11 +313,11 @@
           ctx.arc(
             OFFSET_X + (c * CELL) + (CELL / 2),
             OFFSET_Y + (r * CELL) + (CELL / 2),
-            (piece.name == "pawn" ? 15 : 20),
+            15,
             0,
             2 * Math.PI
           );
-          ctx.fillStyle = piece.team == 1 ? "#fff" : "#000";
+          ctx.fillStyle = piece.team == WHITE ? "#fff" : "#000";
           ctx.fill();
 
           ctx.globalAlpha = 1.0
@@ -296,13 +339,13 @@
 
     ctx.font = "16px Arial";
     ctx.fillStyle = "#000";
-    ctx.fillText("Turn: " + message, 300, 100);
+    ctx.fillText("Turn: " + message, 300, 20);
   }
 
   function drawMessage() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#000";
-    ctx.fillText(message, 300, 120);
+    ctx.fillText(message, 300, 40);
   }
 
   function drawEnd() {
@@ -310,7 +353,7 @@
 
     ctx.font = "16px Arial";
     ctx.fillStyle = "#000";
-    ctx.fillText(message, 300, 100);
+    ctx.fillText(message, 300, 60);
   }
 
   /* control */
